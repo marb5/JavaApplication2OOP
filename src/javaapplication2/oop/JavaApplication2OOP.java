8b9aaedfb392db9a94453c0ec03d90a00cb8d751
Marcin <mbielen1995@gmail.com>
2021-01-21 16:54:12 +0100

    Static variables and methods

--- src/javaapplication2/oop/JavaApplication2OOP.java
+++ src/javaapplication2/oop/JavaApplication2OOP.java
@@ -26,6 +26,16 @@ public class JavaApplication2OOP {
         
         System.out.println(test.add(2, 3));
         System.out.println(test.add(2.1, 3.4));
+        
+        MyMath newMath = new MyMath();
+        System.out.println(MyMath.add(3, 1));
+        System.out.println(MyMath.PI);
+        
+        Client a = new Client("Marcin");
+        Client b = new Client("Ryszard");
+        System.out.println(a.id);
+        System.out.println(b.id);
+        System.out.println(Client.nextId);
     }
     
 }
@@ -40,4 +50,26 @@ class Monitor {
     void turnOff() {
         
     }
+}
+
+class MyMath
+{
+    static final double PI = 3.14; //unchangeable final value
+    static double add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+class Client
+{
+    Client(String name)
+    {
+        this.name = name;       
+        id = nextId;
+        nextId++;
+    }
+    String name;
+    int id = 0;
+    static int nextId = 1;
 }
